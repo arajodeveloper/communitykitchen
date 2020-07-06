7bc327a98c857490eeb111ea5fccf56fd46e7ff7
Ara Jo <arajodeveloper@gmail.com>
2020-07-05 20:08:10 -0700

    did not do map but worked on route

---
+++ app/javascript/components/NeedFood.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class NeedFood extends Component {
+  render() {
+    return (
+      <>
+      <div>
+        This is NeedFood form
+      </div>
+      </>
+    );
+  }
+}
+
+export default NeedFood;
